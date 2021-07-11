91926bc3be02dde4bd7711311b016aae112a3132
Brock Bold <veranis24@gmail.com>
2021-07-11 22:10:16 +0200

    update

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello Worldentent!", {
+  const response = new Response("Hello Worldententg!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
